71b40852bbd848d2230b8bfc7cbb357a7844cd93
ismail <ismail95190@gmail.com>
2020-08-24 21:21:10 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,116 @@
+
+// api key : 82005d27a116c2880c8f0fcb866998a0
+
+// SELECT ELEMENTS
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+const itemsearch=document.getElementById("item1");
+const itemsubmit=document.getElementById("item2");
+let btn=document.getElementById("call");
+let btnrestart=document.getElementById("restart");
+
+// App data
+const weather = {};
+
+weather.temperature = {
+    unit : "celsius"
+}
+
+// APP CONSTS AND VARS
+const KELVIN = 273;
+// API KEY
+const key = "5c80051c6c9951eabbac714852d2d1c7";
+
+
+
+//ADD Event
+btn.addEventListener("click",function(){
+    itemsearch.style.display="none";
+    itemsubmit.style.display="block";
+    const city =document.getElementById("city").value;
+    getWeather(city);
+
+
+
+})
+
+btnrestart.addEventListener("click",function(){
+ 
+        itemsearch.style.display="block";
+        itemsubmit.style.display="none";
+    
+   
+})
+
+
+
+
+
+// SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+
+// GET WEATHER FROM API PROVIDER
+function getWeather(input){
+    let api = `http://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${key}`;
+    
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        });
+}
+
+// DISPLAY WEATHER TO UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+
+
+
+// C to F conversion
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+
+
+
+
+
+
+// WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENET
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+    
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius";
+    }
+});
+
